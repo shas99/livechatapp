4f7edf0b431377b1fd0f08bed6434ab1ef05221b
shas99 <pshasvathan1999@gmail.com>
2025-03-22 01:55:52 +0530

    load balancer test for socket connection

--- chatappFE/hooks/useSocket.ts
+++ chatappFE/hooks/useSocket.ts
@@ -2,7 +2,8 @@
 import { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000'; // Adjust if needed
+// const SOCKET_URL = 'http://localhost:5000'; // Adjust if needed
+const SOCKET_URL = 'http://livechatappnest-565007551.us-east-2.elb.amazonaws.com'; // Adjust if needed
 
 export function useSocket() {
     const [socket, setSocket] = useState<Socket | null>(null); // ✅ Correct typing
