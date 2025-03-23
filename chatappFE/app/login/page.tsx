6d8a0d429d3498f4a62c0494cdb3091237d10b9a
shas99 <pshasvathan1999@gmail.com>
2025-03-23 15:53:14 +0530

    removing token rendering

--- chatappFE/app/login/page.tsx
+++ chatappFE/app/login/page.tsx
@@ -19,9 +19,6 @@ export default function Page() {
       return (
         <div>
           <pre> Hello: {auth.user?.profile.email} </pre>
-          <pre> ID Token: {auth.user?.id_token} </pre>
-          <pre> Access Token: {auth.user?.access_token} </pre>
-          <pre> Refresh Token: {auth.user?.refresh_token} </pre>
   
           <button onClick={() => auth.removeUser()}>Sign out</button>
         </div>
